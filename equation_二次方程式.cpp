1b113f7ade182508f4915e4cfb3215729ee70022
stoq <morihito0118@ezweb.ne.jp>
2020-10-02 17:44:01 +0900

    add new file

--- equation_二次方程式.cpp
+++ equation_二次方程式.cpp
@@ -1,6 +1,7 @@
 // Ax^2 + Bx + C = 0
 // 解の個数(無数にある場合3)、解
-tuple<int, ld, ld> solve(ld A, ld B, ld C)
+using Real = double;
+tuple<int, Real, Real> solve(Real A, Real B, Real C)
 {
   if (fabs(A) < EPS)
   {
@@ -11,20 +12,20 @@ tuple<int, ld, ld> solve(ld A, ld B, ld C)
       else
         return {0, 0, 0};
     }
-    ld ans = -C / B;
+    Real ans = -C / B;
     return {1, ans, 0};
   }
-  ld D = B * B - 4.0 * A * C;
+  Real D = B * B - 4.0 * A * C;
   if (D < 0)
     return {0, 0, 0};
-  ld ans1;
+  Real ans1;
   if (B > 0)
     ans1 = (-B - sqrtl(D)) / 2.0 / A;
   else
     ans1 = (-B + sqrtl(D)) / 2.0 / A;
   if (fabs(D) < EPS)
     return {1, ans1, 0};
-  ld ans2 = C / (A * ans1);
+  Real ans2 = C / (A * ans1);
   if (ans1 > ans1)
     swap(ans1, ans2);
   return {2, ans1, ans2};
